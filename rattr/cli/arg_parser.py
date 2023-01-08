3cf78d6d2eb96bd628b56946fc76f41cf2366824
habospace <avatarokwow@gmail.com>
2023-01-09 00:33:50 +0100

    add overwritten ArgParser class that takes 'exit_on_error' kwarg and only exits when it's set to True

---
+++ rattr/cli/arg_parser.py
@@ -0,0 +1,52 @@
+import sys as _sys
+from argparse import (
+    _UNRECOGNIZED_ARGS_ATTR,
+    SUPPRESS,
+    ArgumentError,
+    ArgumentParser,
+    Namespace,
+)
+
+
+class _ArgumentParser(ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        super(_ArgumentParser, self).__init__(*args, **kwargs)
+        self.exit_on_error = kwargs.get("exit_on_error", False)
+
+    def parse_known_args(self, args=None, namespace=None):
+        if args is None:
+            # args default to the system args
+            args = _sys.argv[1:]
+        else:
+            # make sure that args are mutable
+            args = list(args)
+
+        # default Namespace built from parser defaults
+        if namespace is None:
+            namespace = Namespace()
+
+        # add any action defaults that aren't present
+        for action in self._actions:
+            if action.dest is not SUPPRESS:
+                if not hasattr(namespace, action.dest):
+                    if action.default is not SUPPRESS:
+                        setattr(namespace, action.dest, action.default)
+
+        # add any parser defaults that aren't present
+        for dest in self._defaults:
+            if not hasattr(namespace, dest):
+                setattr(namespace, dest, self._defaults[dest])
+
+        # parse the arguments and exit if there are any errors
+        if self.exit_on_error:
+            try:
+                namespace, args = self._parse_known_args(args, namespace)
+            except ArgumentError as err:
+                self.error(str(err))
+        else:
+            namespace, args = self._parse_known_args(args, namespace)
+
+        if hasattr(namespace, _UNRECOGNIZED_ARGS_ATTR):
+            args.extend(getattr(namespace, _UNRECOGNIZED_ARGS_ATTR))
+            delattr(namespace, _UNRECOGNIZED_ARGS_ATTR)
+        return namespace, args
