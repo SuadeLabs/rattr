7597fa5bfbf1c8913c5bb85e8bc28335615911fd
brandon <brandon@suade.org>
2023-06-13 14:56:52 +0100

    ci: switch from flake8 to ruff on pipeline

--- setup.py
+++ setup.py
@@ -28,9 +28,7 @@ setup(
         "dev": [
             "black==22.1.0",
             "click==8.0.2",
-            "flake8-bugbear==22.1.11",
-            "flake8==4.0.1",
-            "flask==2.0.3",
+            "ruff==0.0.270",
             "isort==5.10.1",
             "pytest==7.0.1",
         ],
